7609e4452d11afc62ab0bee258e186e1b5f5b868
gpinto8 <gpinto0910@gmail.com>
2024-07-31 15:06:57 +0200

    fix images not loading

--- utils/img-urls.ts
+++ utils/img-urls.ts
@@ -1,26 +1,26 @@
 export const IMG_URLS = {
   FUTBOLY_LOGO: {
-    src: '/assets/futboly-logo.svg',
+    src: 'assets/futboly-logo.svg',
     alt: 'Futboly Logo',
   },
   LOGIN_ILLUSTRATION: {
-    src: '/assets/login-illustration.svg',
+    src: 'assets/login-illustration.svg',
     alt: 'Login Illustration',
   },
   AT_LOGO: {
-    src: '/assets/at.svg',
+    src: 'assets/at.svg',
     alt: 'At Logo',
   },
   EYE_OPEN_LOGO: {
-    src: '/assets/eye-open.svg',
+    src: 'assets/eye-open.svg',
     alt: 'Eye Open Logo',
   },
   EYE_CLOSE_LOGO: {
-    src: '/assets/eye-close.svg',
+    src: 'assets/eye-close.svg',
     alt: 'Eye Close Logo',
   },
   USER_LOGO: {
-    src: '/assets/user.svg',
+    src: 'assets/user.svg',
     alt: 'User Logo',
   },
 };
