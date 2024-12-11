e534f365463509c70966e7752af40e6ec1e20244
gpinto8 <gpinto0910@gmail.com>
2024-12-11 02:54:17 +0100

    fix no-leagues pages responsiveness and spacing

--- components/first-time.tsx
+++ components/first-time.tsx
@@ -1,34 +1,43 @@
 'use client';
 
-import { CustomImage } from "@/components/custom/custom-image";
-import { CreateLeagueModal } from "@/components/modal/leagues-modal";
-import { useBreakpoint } from "@/utils/use-breakpoint";
-import { LeagueList } from "@/components/league-list";
-import { CustomSeparator } from "@/components/custom/custom-separator";
+import { CustomImage } from '@/components/custom/custom-image';
+import { CreateLeagueModal } from '@/components/modal/leagues-modal';
+import { useBreakpoint } from '@/utils/use-breakpoint';
+import { LeagueList } from '@/components/league-list';
+import { CustomSeparator } from '@/components/custom/custom-separator';
 
-const FirstTime = () => {
-    return (
-        <div className="mt-12 md:mt-4 min-w-full h-full"> 
-            <div className="flex flex-row gap-4">
-                <div className="flex flex-col justify-center items-start gap-2">
-                    <h1 className="text-3xl font-bold">Create a League</h1>
-                    <div className="flex flex-col gap-4 justify-center">
-                        <p className="text-sm">Play with your friends with a <strong>private league</strong> or make new ones with a <strong>public league!</strong></p>
-                        <CreateLeagueModal buttonFull={false} />
-                    </div>
-                    <CustomSeparator withText={true} text="OR" />
-                    <h1 className="text-3xl font-bold">Join a League</h1>
-                    <p>Choose one from the list below or join a league</p>
-                </div>
-                {useBreakpoint() !== "sm" && (
-                    <div>
-                        <CustomImage imageKey="LEAGUE_ILLUSTRATION" width={500} height={500} />
-                    </div>
-                )}
-            </div>
-            <LeagueList/>
+const FirstTime = () => (
+  <div className="mt-12 min-w-full h-full">
+    <div className="flex pb-4 flex-row gap-4 justify-between items-end">
+      <div className="flex flex-col">
+        <div className="flex flex-col justify-center items-start gap-2">
+          <h1 className="text-3xl font-bold">Create a League</h1>
+          <div className="flex flex-col gap-4 justify-center">
+            <p className="text-sm">
+              Play with your friends with a <strong>private league</strong> or
+              make new ones with a <strong>public league!</strong>
+            </p>
+            <CreateLeagueModal buttonFull={false} />
+          </div>
         </div>
-    );
-}
+        <CustomSeparator withText={true} text="OR" />
+        <div className="flex flex-col justify-center items-start gap-2">
+          <h1 className="text-3xl font-bold">Join a League</h1>
+          <p>Choose one from the list below or join a league</p>
+        </div>
+      </div>
+      {useBreakpoint() !== 'sm' && (
+        <div>
+          <CustomImage
+            imageKey="LEAGUE_ILLUSTRATION"
+            width={300}
+            height={300}
+          />
+        </div>
+      )}
+    </div>
+    <LeagueList />
+  </div>
+);
 
 export default FirstTime;
