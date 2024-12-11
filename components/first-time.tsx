365908b9f54c66653a46bb1302a02a6caeb6f48d
gpinto8 <gpinto0910@gmail.com>
2024-12-11 02:19:24 +0100

    remove exceeding no-league page spacing + header

--- components/first-time.tsx
+++ components/first-time.tsx
@@ -8,11 +8,11 @@ import { CustomSeparator } from "@/components/custom/custom-separator";
 
 const FirstTime = () => {
     return (
-        <div className="mt-4 min-w-full px-4 md:px-8 lg:px-12 h-full"> 
+        <div className="mt-12 md:mt-4 min-w-full h-full"> 
             <div className="flex flex-row gap-4">
                 <div className="flex flex-col justify-center items-start gap-2">
                     <h1 className="text-3xl font-bold">Create a League</h1>
-                    <div className="flex flex-col md:flex-row gap-4 justify-center items-center">
+                    <div className="flex flex-col gap-4 justify-center">
                         <p className="text-sm">Play with your friends with a <strong>private league</strong> or make new ones with a <strong>public league!</strong></p>
                         <CreateLeagueModal buttonFull={false} />
                     </div>
--- components/header.tsx
+++ components/header.tsx
@@ -47,88 +47,84 @@ export const Header = ({
   const openUser = Boolean(anchorElUser);
   const idRules = openRules ? 'rules-popover' : undefined;
   const idUser = openUser ? 'user-popover' : undefined;
-  // const logoWidth = 160;
 
   return (
-    <nav>
-      <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-6 pt-4">
-        <div className="relative flex h-16 items-center justify-between">
-          <FutbolyLogo />
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 gap-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            {/* <a href="/first-time">Prova</a> */}
-            {/* USERNAME */}
-            {username && !hideUsername && (
-              <div className="text-lg">
-                Hello, <span className="font-bold">{username}</span>
-              </div>
-            )}
-            {!hideRules && (
-              <>
-                <button
-                  onClick={handleClickRules}
-                  type="button"
-                  className="relative rounded-full p-1 text-gray-400 hover:text-white hover:bg-gray-100 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <CustomImage
-                    imageKey="RULES_ICON"
-                    className="h-9 w-9"
-                    width={36}
-                    height={36}
-                  />
-                </button>
-                <RulesPopover
-                  id={idRules}
-                  open={openRules}
-                  anchorEl={anchorElRules}
-                  functions={{ handleClose: handleCloseRules }}
+    <div className="flex flex-col pt-6">
+      <div className="flex flex-wrap gap-4 w-full justify-between">
+        <FutbolyLogo />
+        <div className="flex items-center gap-2">
+          {/* USERNAME */}
+          {username && !hideUsername && (
+            <div className="text-lg">
+              Hello, <span className="font-bold">{username}</span>
+            </div>
+          )}
+          {!hideRules && (
+            <>
+              <button
+                onClick={handleClickRules}
+                type="button"
+                className="relative rounded-full p-1 text-gray-400 hover:text-white hover:bg-gray-100 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+              >
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">View notifications</span>
+                <CustomImage
+                  imageKey="RULES_ICON"
+                  className="h-9 w-9"
+                  width={36}
+                  height={36}
                 />
-              </>
-            )}
-
-            {/* Profile dropdown */}
-            {!hideUserPopover && (
-              <>
-                <button
-                  onClick={handleClickUser}
-                  type="button"
-                  className="relative rounded-full p-1 text-gray-400 hover:text-white hover:bg-gray-100 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <CustomImage
-                    imageKey="USER_ICON"
-                    className="h-8 w-8"
-                    width={32}
-                    height={32}
-                  />
-                </button>
-                <UserPopover
-                  id={idUser}
-                  open={openUser}
-                  anchorEl={anchorElUser}
-                  functions={{ handleClose: handleCloseUser }}
-                />
-              </>
-            )}
-            <button
-              onClick={logoutUser}
-              type="button"
-              className="relative rounded-full p-1 text-gray-400 hover:text-white hover:bg-gray-100 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
-            >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">Logout</span>
-              <CustomImage
-                imageKey="LOGOUT"
-                className="h-8 w-8"
-                width={32}
-                height={32}
+              </button>
+              <RulesPopover
+                id={idRules}
+                open={openRules}
+                anchorEl={anchorElRules}
+                functions={{ handleClose: handleCloseRules }}
               />
-            </button>
-          </div>
+            </>
+          )}
+
+          {/* Profile dropdown */}
+          {!hideUserPopover && (
+            <>
+              <button
+                onClick={handleClickUser}
+                type="button"
+                className="relative rounded-full p-1 text-gray-400 hover:text-white hover:bg-gray-100 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+              >
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">View notifications</span>
+                <CustomImage
+                  imageKey="USER_ICON"
+                  className="h-8 w-8"
+                  width={32}
+                  height={32}
+                />
+              </button>
+              <UserPopover
+                id={idUser}
+                open={openUser}
+                anchorEl={anchorElUser}
+                functions={{ handleClose: handleCloseUser }}
+              />
+            </>
+          )}
+          <button
+            onClick={logoutUser}
+            type="button"
+            className="relative rounded-full p-1 text-gray-400 hover:text-white hover:bg-gray-100 active:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+          >
+            <span className="absolute -inset-1.5" />
+            <span className="sr-only">Logout</span>
+            <CustomImage
+              imageKey="LOGOUT"
+              className="h-8 w-8"
+              width={32}
+              height={32}
+            />
+          </button>
         </div>
       </div>
-    </nav>
+    </div>
   );
 };
