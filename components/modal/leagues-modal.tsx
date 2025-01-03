8f98d57273576bf5c516be8acc1ed9c7572b69d0
gpinto8 <gpinto0910@gmail.com>
2025-01-03 02:12:19 +0100

    fix create league logic and refactor code (for both private and public) + add disabled button when inputs are empty + reduce checkbox width

--- components/modal/leagues-modal.tsx
+++ components/modal/leagues-modal.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { useState } from 'react';
-import { CustomModal } from "@/components/custom/custom-modal";
-import { CustomInput, InputProps } from "@/components/custom/custom-input";
+import { CustomModal } from '@/components/custom/custom-modal';
+import { CustomInput, InputProps } from '@/components/custom/custom-input';
 import { InputPassword } from '@/components/input/input-password';
-import { useSetLeague } from "@/data/leagues/use-set-league";
+import { useSetLeague } from '@/data/leagues/use-set-league';
 import { useAppSelector } from '@/store/hooks';
 import { LeaguesCollectionProps } from '@/firebase/db-types';
 import { LeagueList } from '@/components/league-list';
@@ -13,56 +13,69 @@ import { LeagueList } from '@/components/league-list';
 type HandleChangeParamProps = Parameters<InputProps['handleChange']>[0];
 
 export const CreateLeagueModal = ({ buttonFull }) => {
-    const [leagueName, setLeagueName] = useState<HandleChangeParamProps>();
-    const [leaguePassword, setLeaguePassword] = useState<HandleChangeParamProps>();
-    const [repeatPassword, setRepeatPassword] = useState<HandleChangeParamProps>();
-    const [resetForm, setResetForm] = useState(false);
-    const [isPrivate, setIsPrivate] = useState(false);
-    const { addLeague } = useSetLeague();
+  const [leagueName, setLeagueName] = useState<HandleChangeParamProps>();
+  const [leaguePassword, setLeaguePassword] =
+    useState<HandleChangeParamProps>();
+  const [repeatPassword, setRepeatPassword] =
+    useState<HandleChangeParamProps>();
+  const [resetForm, setResetForm] = useState(false);
+  const [isPrivate, setIsPrivate] = useState(false);
+  const { addLeague } = useSetLeague();
 
-    const openButtonStyle = (!buttonFull ? "max-w-20"  : "") + " rounded-full mb-2 px-20";
+  const openButtonStyle =
+    (!buttonFull ? 'max-w-20' : '') + ' rounded-full mb-2 px-20';
 
-    const handleSubmit = async () => {
-        if (!leagueName || !leaguePassword || !repeatPassword) return;  //Se uno dei valori è vuoto
-        if (!(leagueName.isValid && leaguePassword.isValid && repeatPassword.isValid)) return; //Uno dei valore non è valido
-        if (leaguePassword.value !== repeatPassword.value) return; //Le password non corrispondono
-        // const isPrivate = (document.getElementById("isPrivate") as HTMLInputElement).checked;
-        const response = await addLeague({
-            name: leagueName.value,
-            leaguePassword: leaguePassword.value,
-            isPrivate
-        });
-        if (response) {
-            setResetForm(true);
-            setResetForm(false);
-        }
+  const handleSubmit = async () => {
+    let response: any;
+
+    const { value, isValid } = leagueName || {};
+    if (!value || !isValid) return;
+
+    // PRIVATE LEAGUE
+    if (isPrivate && leaguePassword && repeatPassword) {
+      if (leaguePassword.value !== repeatPassword.value) return; // If password dont correspond
+      if (!(leaguePassword.isValid && repeatPassword.isValid)) return; // If both password are not valid
+
+      response = await addLeague({
+        name: value,
+        isPrivate,
+        leaguePassword: leaguePassword.value,
+      });
     }
+    // NON-PRIVATE LEAGUE
+    else response = await addLeague({ name: value, isPrivate });
 
-    const toggleIsPrivate = (event: any) => {
-        setIsPrivate(event.target.checked);
+    if (response) {
+      setResetForm(true);
+      setResetForm(false);
     }
+  };
 
-    return (
-      <CustomModal
-        title={<p className="text-3xl font-bold">Create Your League</p>}
-        closeButton={{ label: 'Create' }}
-        openButton={{
-          label: 'Create',
-          style: 'main',
-          className: openButtonStyle,
-        }}
-        handleClose={handleSubmit}
-        isDialog={{ value: true, style: 'large' }}
-      >
-        <div className="flex flex-col gap-4 mt-2">
-          <div className="flex flex-col items-center justify-center gap-4">
-            <CustomInput
-              label="League Name"
-              handleChange={setLeagueName}
-              endAdorment={{ img: 'LEAGUE_TROPHY' }}
-            />
-          </div>
-          { isPrivate && (
+  const toggleIsPrivate = (event: any) => {
+    setIsPrivate(event.target.checked);
+  };
+
+  return (
+    <CustomModal
+      title={<p className="text-3xl font-bold">Create Your League</p>}
+      closeButton={{ label: 'Create', disabled: !leagueName?.value }}
+      openButton={{
+        label: 'Create',
+        style: 'main',
+        className: openButtonStyle,
+      }}
+      handleClose={handleSubmit}
+      isDialog={{ value: true, style: 'large' }}
+    >
+      <div className="flex flex-col gap-4 mt-2">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <CustomInput
+            label="League Name"
+            handleChange={setLeagueName}
+            endAdorment={{ img: 'LEAGUE_TROPHY' }}
+          />
+        </div>
+        {isPrivate && (
           <div className="flex flex-col md:flex-row items-center justify-center gap-4 ">
             <InputPassword
               label="League Password"
@@ -75,67 +88,110 @@ export const CreateLeagueModal = ({ buttonFull }) => {
               resetValue={resetForm}
             />
           </div>
-          )}
-          <div className="w-full">
-            <label htmlFor="isPrivate" className="flex gap-4" onChange={toggleIsPrivate}>
-              <input type="checkbox" id="isPrivate" name="isPrivate" className="size-5 rounded-md border-gray-200 bg-white shadow-sm" />
-              <span className="text-sm text-gray-700 text-nowrap">Private</span>
-            </label>
-          </div>
+        )}
+        <div className="w-fit">
+          <label
+            htmlFor="isPrivate"
+            className="flex gap-2"
+            onChange={toggleIsPrivate}
+          >
+            <input
+              type="checkbox"
+              id="isPrivate"
+              name="isPrivate"
+              className="size-5 rounded-md border-gray-200 bg-white shadow-sm"
+            />
+            <span className="text-sm text-gray-700 text-nowrap">Private</span>
+          </label>
         </div>
-      </CustomModal>
-    );
+      </div>
+    </CustomModal>
+  );
 };
 
-export const JoinPublicLeagueModal = ({league}: {league: LeaguesCollectionProps}) => {
-    const [leaguePasswordInput, setLeaguePasswordInput] = useState<HandleChangeParamProps | null>(null);
-    const [resetForm, setResetForm] = useState(false);
-    const { addPlayerToLeague } = useSetLeague();
-    const user = useAppSelector((state) => state.user);
+export const JoinPublicLeagueModal = ({
+  league,
+}: {
+  league: LeaguesCollectionProps;
+}) => {
+  const [leaguePasswordInput, setLeaguePasswordInput] =
+    useState<HandleChangeParamProps | null>(null);
+  const [resetForm, setResetForm] = useState(false);
+  const { addPlayerToLeague } = useSetLeague();
+  const user = useAppSelector((state) => state.user);
 
-    const handleClick = async () => {
-        // console.log("join", league)
-        if (league?.isPrivate) {
-            //Verificare che la pw sia corretta
-            if (leaguePasswordInput?.value === league?.leaguePassword) {
-                await addPlayerToLeague(league.id, user.id);
-            }
-        } else {
-            await addPlayerToLeague(league.id, user.id);
-        }
-        setResetForm(true);
+  const handleClick = async () => {
+    if (league?.isPrivate) {
+      //Verificare che la pw sia corretta
+      if (leaguePasswordInput?.value === league?.leaguePassword) {
+        await addPlayerToLeague(league.id, user.id);
+      }
+    } else {
+      await addPlayerToLeague(league.id, user.id);
     }
+    setResetForm(true);
+  };
 
-    const handleClose = () => {
-        setResetForm(true);
-        setLeaguePasswordInput(null);
-        setResetForm(false);
-    }
+  const handleClose = () => {
+    setResetForm(true);
+    setLeaguePasswordInput(null);
+    setResetForm(false);
+  };
 
-    return(
-        <CustomModal title={<p className="text-2xl sm:text-3xl font-bold sm:text-nowrap">Join League</p>} 
-        closeButton={{label: "Join", style: "main", className: "mt-4 rounded-full", handleClick: handleClick}} 
-        openButton={{label: "Join", isText: false, className: "rounded-full text-xs py-1 my-1 px-4 h-full", style: "main"}}
-        handleClose={handleClose}
-        isDialog={{value: true, style: "slim"}}>
-            <div className="flex flex-col gap-4 my-2 min-w-[60vw] md:min-w-[30vw] xl:min-w-[25vw]">
-                <CustomInput label="League ID" initialValue={league.name} disabled/>
-            </div>
-            {league.isPrivate && (
-                <CustomInput label="League Password" type="password" handleChange={setLeaguePasswordInput} resetValue={resetForm} />
-            )}
-        </CustomModal>
-    );
+  return (
+    <CustomModal
+      title={
+        <p className="text-2xl sm:text-3xl font-bold sm:text-nowrap">
+          Join League
+        </p>
+      }
+      closeButton={{
+        label: 'Join',
+        style: 'main',
+        className: 'mt-4 rounded-full',
+        handleClick: handleClick,
+      }}
+      openButton={{
+        label: 'Join',
+        isText: false,
+        className: 'rounded-full text-xs py-1 my-1 px-4 h-full',
+        style: 'main',
+      }}
+      handleClose={handleClose}
+      isDialog={{ value: true, style: 'slim' }}
+    >
+      <div className="flex flex-col gap-4 my-2 min-w-[60vw] md:min-w-[30vw] xl:min-w-[25vw]">
+        <CustomInput label="League ID" initialValue={league.name} disabled />
+      </div>
+      {league.isPrivate && (
+        <CustomInput
+          label="League Password"
+          type="password"
+          handleChange={setLeaguePasswordInput}
+          resetValue={resetForm}
+        />
+      )}
+    </CustomModal>
+  );
 };
 
 export const LeaguesModal = () => {
-    return (
-        <CustomModal title={<p className="text-2xl sm:text-3xl font-bold sm:text-nowrap">Leagues</p>} 
-        openButton={{label: "Browse Leagues", isText: false, className: "rounded-full text-xs py-1 my-1 px-4", style: "black"}}
-        closeButton={{label: "Close", style: "main"}}
-        isDialog={{value: true, style: "large"}}
-        className='min-w-[95%]'>
-            <LeagueList />
-        </CustomModal>
-    );
+  return (
+    <CustomModal
+      title={
+        <p className="text-2xl sm:text-3xl font-bold sm:text-nowrap">Leagues</p>
+      }
+      openButton={{
+        label: 'Browse Leagues',
+        isText: false,
+        className: 'rounded-full text-xs py-1 my-1 px-4',
+        style: 'black',
+      }}
+      closeButton={{ label: 'Close', style: 'main' }}
+      isDialog={{ value: true, style: 'large' }}
+      className="min-w-[95%]"
+    >
+      <LeagueList />
+    </CustomModal>
+  );
 };
--- data/leagues/use-set-league.ts
+++ data/leagues/use-set-league.ts
@@ -3,8 +3,17 @@ import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { leagueActions } from '@/store/slices/league';
 import { useGetLeagues } from './use-get-leagues';
 import { getShortBase64Id } from '@/utils/id';
-import { LeaguesCollectionProps, MappedLeaguesProps } from '@/firebase/db-types';
+import {
+  LeaguesCollectionProps,
+  MappedLeaguesProps,
+} from '@/firebase/db-types';
 import { DocumentReference } from 'firebase/firestore';
+
+type LeagueFormDetailsProps = {
+  name: string;
+  isPrivate: boolean;
+  leaguePassword?: string;
+};
 
 export const useSetLeague = () => {
   const { getLeagueById } = useGetLeagues();
@@ -12,37 +21,37 @@ export const useSetLeague = () => {
   const user = useAppSelector((state) => state.user); // Can't use the "useGetUsers" hook because it creates an infinite loop since it uses the this hook
 
   const addLeague = async (leagueProps: LeagueFormDetailsProps) => {
-    if (!leagueProps.name || !leagueProps.leaguePassword) return;
-    //TODO Verify if the league name is unique
-    if (!user.id || !user.username) return;
+    if (!leagueProps.name) return;
+    if (!user.id || !user.username) return; //TODO Verify if the league name is unique
+
     const leagueData = await firestoreMethods('leagues', 'id').createDocument({
       ...leagueProps,
       owner: user.id,
       ownerUsername: user.username,
       competitions: [],
-      players: { [user.id]: "owner" },
+      players: { [user.id]: 'owner' },
     });
+
     const leagueShortID = getShortBase64Id();
     if (leagueShortID === 'Error') return; //Handle better the error re trying or using another ID
+
     const updateResult = await firestoreMethods(
       'leagues',
       leagueData.id,
     ).createField('shortId', leagueShortID);
     if (!updateResult) return;
+
     leagueData.shortId = leagueShortID;
 
     const leagueRef = firestoreMethods('leagues', leagueData.id).getDocRef();
-    // console.log({ leagueRef });
-    const updateUser = await firestoreMethods(
-      'users',
-      user.id as any,
-    ).addDataToField('leagues', leagueRef, 'array');
+    await firestoreMethods('users', user.id as any).addDataToField(
+      'leagues',
+      leagueRef,
+      'array',
+    );
 
-    // console.log({ updateUser });
-    // await setLeague(playerId);
     location.reload(); // TODO: set to redux the updated league data
 
-    // dispatch(leagueActions.setLeague(leagueData));
     return leagueData;
   };
 
@@ -52,57 +61,49 @@ export const useSetLeague = () => {
     playerId: string, // TODO: name better the user playerId -> userId
   ) => {
     if (!leagueId || !playerId || !league) return;
-    const newPlayers = Object.entries(league.players).filter(([player, role]) => player !== playerId);
+
+    const newPlayers = Object.entries(league.players).filter(
+      ([player, role]) => player !== playerId,
+    );
+
     if (newPlayers.length === 0) {
-      const updateResult = await firestoreMethods(
-        'leagues',
-        leagueId as any,
-      ).deleteDocument();
-      console.log(updateResult);
+      await firestoreMethods('leagues', leagueId as any).deleteDocument();
     } else {
-      const updateResult = await firestoreMethods(
-        'leagues',
-        leagueId as any,
-      ).replaceField('players', newPlayers);
-      console.log(updateResult);
+      await firestoreMethods('leagues', leagueId as any).replaceField(
+        'players',
+        newPlayers,
+      );
     }
+
     location.reload(); // TODO: set to redux the updated league data
   };
 
   const addPlayerToLeague = async (leagueId: string, playerId: string) => {
     if (!leagueId || !playerId) return;
-    const updateLeague = await firestoreMethods(
-      'leagues',
-      leagueId as any,
-    ).createField(playerId, "guest");
-    const leagueRef = firestoreMethods('leagues', leagueId as any).getDocRef();
+
+    await firestoreMethods('leagues', leagueId as any).createField(
+      playerId,
+      'guest',
+    );
+
+    firestoreMethods('leagues', leagueId as any).getDocRef();
+
     location.reload(); // TODO: set to redux the updated league data
   };
 
   // SET LEAGUE TO REDUX FROM THE USER ID --> Here I can pass directly the League object, avoid the fetch and dispatch directly the league with a "random" competition
-  const setLeague = async (league: MappedLeaguesProps /*| DocumentReference<LeaguesCollectionProps>*/, uid: string) => {
-    // console.log({ uid });
-    
-    // const data = await getLeagueById(id);
+  const setLeague = async (league: MappedLeaguesProps, uid: string) => {
+    // Update the user active league
+    const leagueRef = firestoreMethods('leagues', league.id as any).getDocRef();
+    if (!leagueRef) return;
 
-    // if (data) {
-      // Update the user active league
-      const leagueRef = firestoreMethods('leagues', league.id as any).getDocRef();
-      if (!leagueRef) return;
-      const userUpdate = await firestoreMethods('users', uid as any).replaceRefField("activeLeague", leagueRef); // Using data.id and not just id for "safety"
+    const userUpdate = await firestoreMethods(
+      'users',
+      uid as any,
+    ).replaceRefField('activeLeague', leagueRef); // Using data.id and not just id for "safety"
 
-      if (userUpdate) {
-        dispatch(leagueActions.setLeague(league));  //TODO: Dispatch also the updated user info
-      }
-    // }
+    if (userUpdate) dispatch(leagueActions.setLeague(league)); //TODO: Dispatch also the updated user info
   };
 
   return { addLeague, exitLeague, setLeague, addPlayerToLeague };
-};
-
-type LeagueFormDetailsProps = {
-  name: string;
-  leaguePassword: string;
-  // specificPosition: boolean; Now this prop is defined in the Competition
-  isPrivate: boolean;
 };
