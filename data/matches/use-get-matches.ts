44c122eed5e7dfeb153044c1977e08f984c92d9f
Ciro Vavotici <ciro.vavotici@gmail.com>
2025-03-27 22:53:06 +0100

    Fixed field coach and matches were 'froze' so with the ... op I create a copy

--- data/matches/use-get-matches.ts
+++ data/matches/use-get-matches.ts
@@ -12,14 +12,14 @@ export const useGetMatches = () => {
     // Return personal matches ordered by week
     const getPersonalMatches = () => {
         if (!matches) return [];
-        return matches.filter((match) => (match.home.coach === user.username || match.away.coach === user.username)).sort((a, b) => a.week - b.week);
+        return [...matches].filter((match) => (match.home.ownerUsername === user.username || match.away.ownerUsername === user.username)).sort((a, b) => a.week - b.week);
     };
 
     const getMatchStatistics = () => {
         const personalMatches = getPersonalMatches();
         let totalWins = 0, totalMatchPlayed = 0, totalScore = 0, scoredThisWeek = 0;
         personalMatches.forEach((personalMatch) => {
-            const matchSide = personalMatch.home.coach === user.username ? "Home" : "Away";
+            const matchSide = personalMatch.home.ownerUsername === user.username ? "Home" : "Away";
             if (personalMatch.result) {
                 totalMatchPlayed++; // If match has a result 
                 const winnerSide = personalMatch.result.home > personalMatch.result.away ? "Home" :  personalMatch.result.home !== personalMatch.result.away ? "Away" : "Draw"; 
@@ -39,7 +39,7 @@ export const useGetMatches = () => {
     // Return all the matches but sorted by week
     const getAllMatches = () => {
         if (!matches) return [];
-        return matches.sort((a, b) => a.week - b.week);
+        return [...matches].sort((a, b) => a.week - b.week);
     };
 
     return {
