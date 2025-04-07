2559043d8dda09dc9c5ab8a4035d6e23431b0406
Ciro Vavotici <ciro.vavotici@gmail.com>
2025-04-07 23:57:56 +0200

    Added state for variables to useEffect on competition change

--- components/matches-tab.tsx
+++ components/matches-tab.tsx
@@ -1,17 +1,26 @@
+import { useEffect, useState } from "react";
 import { useGetMatches } from '@/data/matches/use-get-matches';
 import { NoMatches } from '@/components/tabs/matches-tab/no-matches';
 import { Matches } from '@/components/tabs/matches-tab/matches';
+import { useAppSelector } from '@/store/hooks';
 
 export const MatchesTab = () => {
+    const competition = useAppSelector((state) => state.competition);
   const { getPersonalMatches, getAllMatches, getMatchStatistics } =
     useGetMatches();
-  const personalMatchHistory = getPersonalMatches();
-  const allMatchHistory = getAllMatches();
-  const personalStatistics = getMatchStatistics();
+    const [personalMatchHistory, setPersonalMatchHistory] = useState<any>(null);
+    const [allMatchHistory, setAllMatchHistory] = useState<any>(null);
+    const [personalStatistics, setPersonalStatistics] = useState<any>(null);
+
+    useEffect(() => {
+  setPersonalMatchHistory(getPersonalMatches());
+  setAllMatchHistory(getAllMatches());
+  setPersonalStatistics(getMatchStatistics());
+    }, [competition]);
 
   return (
     <div>
-      {allMatchHistory.length > 0 ? (
+      {allMatchHistory?.length > 0 && personalMatchHistory?.length > 0 ? (
         <Matches
           personalMatchHistory={personalMatchHistory}
           allMatchHistory={allMatchHistory}
