e0830d39119928c53b05e77c81bf500b1cc887e5
Ciro Vavotici <ciro.vavotici@gmail.com>
2025-04-10 00:17:44 +0200

    Showed data for standings

--- components/standings-tab.tsx
+++ components/standings-tab.tsx
@@ -1,7 +1,11 @@
+import { useState, useEffect } from 'react';
 import { ColumnsProps, CustomTable, RowsProps } from './custom/custom-table';
 import { ImageUrlsProps } from '@/utils/img-urls';
 import { useBreakpoint } from '@/utils/use-breakpoint';
 import { CustomImage } from './custom/custom-image';
+import { useGetMatches } from '@/data/matches/use-get-matches';
+import { useGetStandings } from '@/data/standings/use-get-standings';
+import { useAppSelector } from '@/store/hooks';
 
 type ColumnKeysProps =
   | 'INDEX'
@@ -12,18 +16,18 @@ type ColumnKeysProps =
   | 'POINTS'
   | 'LAST_MATCHES';
 
+const icons = {
+    W: 'WIN_CIRCLE_ICON',
+    D: 'DRAW_CIRCLE_ICON',
+    L: 'LOSE_CIRCLE_ICON',
+};
+
 const getLastMatchesIcons = (lastMatches: ('W' | 'D' | 'L')[]) => {
   lastMatches.length = 5; // Limit the UI to just show 5
 
   return (
     <div className="flex gap-1.5 justify-center">
       {lastMatches.map((lastMatch, index) => {
-        const icons = {
-          W: 'WIN_CIRCLE_ICON',
-          D: 'DRAW_CIRCLE_ICON',
-          L: 'LOSE_CIRCLE_ICON',
-        };
-
         if (lastMatch) {
           const icon = icons[lastMatch] as ImageUrlsProps;
           return (
@@ -36,46 +40,32 @@ const getLastMatchesIcons = (lastMatches: ('W' | 'D' | 'L')[]) => {
 };
 
 export const StandingsTab = () => {
+    const user = useAppSelector((state) => state.user);
+    const leagueOwner = useAppSelector(state => state.league.owner);
   const breakpoint = useBreakpoint();
+    const { pastMatchesNotCalculated } = useGetMatches();
+    const { getActiveCompetitionStandings } = useGetStandings();
+    const [ standings, setStandings] = useState<any>(null);
 
-  const rows: RowsProps<ColumnKeysProps> = [
-    {
-      INDEX: 1,
-      TEAM: 'Team1',
-      WINS: 12,
-      DRAWS: 12,
-      LOSES: 12,
-      POINTS: 35,
-      LAST_MATCHES: getLastMatchesIcons(['W', 'L', 'W', 'D', 'W', 'L']),
-    },
-    {
-      INDEX: 1,
-      TEAM: 'Team1',
-      WINS: 12,
-      DRAWS: 12,
-      LOSES: 12,
-      POINTS: 35,
-      LAST_MATCHES: '',
-    },
-    {
-      INDEX: 1,
-      TEAM: 'Team1',
-      WINS: 12,
-      DRAWS: 12,
-      LOSES: 12,
-      POINTS: 35,
-      LAST_MATCHES: '',
-    },
-    {
-      INDEX: 1,
-      TEAM: 'Team1',
-      WINS: 12,
-      DRAWS: 12,
-      LOSES: 12,
-      POINTS: 35,
-      LAST_MATCHES: '',
-    },
-  ];
+    let textForPastMatches = "There are matches that have not been calculated yet. ";
+    textForPastMatches += leagueOwner === user.id ? "Go to Live Match and press Calculate Results" : "Ask the Admin to confirm and save to update the standings";
+    useEffect(() => {
+        (async () => {
+        const _standings = await getActiveCompetitionStandings();    
+        console.log(_standings);
+        setStandings(_standings);
+        })();
+    }, []);
+
+    const rows: RowsProps<ColumnKeysProps> = standings?.map((team, index) => { return {
+        INDEX: index + 1,
+        TEAM: team.name,
+        WINS: team.results.W,
+        DRAWS: team.results.D,
+        LOSES: team.results.L,
+        POINTS: team.results.points,
+        LAST_MATCHES: getLastMatchesIcons(team.results.lastMatches),
+    }});
 
   const columns: ColumnsProps<ColumnKeysProps> = [
     { label: '#', id: 'INDEX', minWidth: 30 },
@@ -113,8 +103,8 @@ export const StandingsTab = () => {
   ];
 
   return (
-    <div className="h-[400px]">
-      TODO
+    <div className="h-[400px] text-center">
+    { pastMatchesNotCalculated() && (<p className="text-error-400 font-semibold mb-4">{textForPastMatches}</p>)}
       <CustomTable<ColumnKeysProps>
         rows={rows}
         columns={columns}
