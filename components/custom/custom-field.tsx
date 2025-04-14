97cf211edcf67b61f281b876746e4886f615e556
Ciro Vavotici <ciro.vavotici@gmail.com>
2025-04-14 03:36:20 +0200

    Changed component to implement also double matchup showing 2 teams

--- components/custom/custom-field.tsx
+++ components/custom/custom-field.tsx
@@ -3,18 +3,67 @@ import { PlayerType } from '@/firebase/db-types';
 import { CustomImage } from '@/components/custom/custom-image';
 
 type CustomFieldProps = {
+    teams: 1 | 2;
   className?: string;
+    player_module: {
   players: PlayerType[];
   module: string;
+    }[];
   id?: string;
+    horizontal?: boolean;
 };
 
 export const CustomField = ({
   id,
-  players,
-  module,
+    player_module,
   className,
+    horizontal,
+    teams
 }: CustomFieldProps) => {
+
+    return player_module.length === 1 && teams === 1 ? (
+        <div
+            id={id}
+            style={{
+                backgroundImage: "url('/assets/gray-field.svg')",
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: '100% 100%',
+            }}
+            className={className}
+        >
+            <TeamSection players={player_module[0].players} module={player_module[0].module} />
+        </div>
+        ) : (
+        <div
+            id={id}
+            style={{
+                backgroundImage: "url('/assets/gray-field.svg')",
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: '100% 100%',
+            }}
+            className={className}
+        >
+            <TeamSection players={player_module[0].players} module={player_module[0].module} isReversed />
+            <TeamSection players={player_module[1].players} module={player_module[1].module} />
+        </div>
+        )
+
+  /*return (
+        <div id="soccerFieldStarting11" className="relative w-full h-full" >
+            <CustomImage imageKey="GRAY_FIELD" className="absolute top-0 left-0 z-0 object- w-full h-full" style={{
+                height: "692px !important",
+                width: "567px !important",
+            }}/>
+            <div className="relative z-1 w-full h-full">
+                CIAO
+            </div>
+        </div>
+    );*/
+};
+
+const TeamSection = ({players, module, isReversed}: {players: PlayerType[], module: string, isReversed?: boolean}) => {
   const [goalKeeper, setGoalKeeper] = useState<PlayerType | null>(null);
   const [defense, setDefense] = useState<PlayerType[]>([]);
   const [midfield, setMidfield] = useState<PlayerType[]>([]);
@@ -31,61 +80,56 @@ export const CustomField = ({
     setForward(copyPlayers.splice(0, Number(module.split('-')[2])));
   }, [players, module]);
 
-  return (
-    <div
-      id={id}
-      className={'w-full h-full grid grid-rows-4 ' + className}
-      style={{
-        backgroundImage: "url('/assets/gray-field.svg')",
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: '100% 100%',
-      }}
-    >
+    const teamComposition: any[] = [];
+    teamComposition.push(
       <div
         id="formation-fw"
+        key={"formation-fw"}
         className="row-span-1 flex w-full h-full justify-around items-center"
       >
         {forward.map((player: any, index) => (
           <PlayerSection key={index} player={player} />
         ))}
       </div>
+      );
+    teamComposition.push(
       <div
         id="formation-mf"
+        key="formation-mf"
         className="row-span-1 flex w-full h-full justify-around items-center"
       >
         {midfield.map((player: any, index) => (
           <PlayerSection key={index} player={player} />
         ))}
       </div>
+    );
+    teamComposition.push(
       <div
         id="formation-def"
+        key="formation-def"
         className="row-span-1 flex w-full h-full justify-around items-center"
       >
         {defense.map((player: any, index) => (
           <PlayerSection key={index} player={player} />
         ))}
       </div>
+        );
+    teamComposition.push(
       <div
         id="formation-gk"
+        key="formation-gk"
         className="row-span-1 flex w-full h-full justify-around items-center"
       >
         <PlayerSection player={goalKeeper} />
       </div>
+        );
+return (
+    <div
+        className={'w-full h-full grid grid-rows-4 '}
+    >
+            { (!isReversed) ? teamComposition.map(jsxElement => jsxElement) : teamComposition.toReversed().map(jsxElement => jsxElement) }
     </div>
   );
-
-  /*return (
-        <div id="soccerFieldStarting11" className="relative w-full h-full" >
-            <CustomImage imageKey="GRAY_FIELD" className="absolute top-0 left-0 z-0 object- w-full h-full" style={{
-                height: "692px !important",
-                width: "567px !important",
-            }}/>
-            <div className="relative z-1 w-full h-full">
-                CIAO
-            </div>
-        </div>
-    );*/
 };
 
 const PlayerSection = ({ player }: { player: PlayerType | null }) => {
@@ -93,6 +137,7 @@ const PlayerSection = ({ player }: { player: PlayerType | null }) => {
     <div className="border border-black p-1 rounded-full bg-white flex flex-col justify-center items-center size-24">
       <CustomImage
         forceSrc={player.image}
+        forcedAlt={"player-image-" + player.name}
         className={
           'size-10 border rounded-full' +
           (player.isCaptain ? ' border-error-500' : '')
